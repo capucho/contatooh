ced7858b642b9e1ed44cdcc1c6822b064e2a9ffd
J. Henrique Capucho <jhcapucho@gmail.com>
2016-10-30 18:26:38 -0200

    starting the form to show/add contact

--- public/js/main.js
+++ public/js/main.js
@@ -17,7 +17,7 @@ function _config( $routeProvider ) {
     controller: 'ContatosController'
   } );
 
-  $routeProvider.when( '/contatos/:contatoId', {
+  $routeProvider.when( '/contato/:contatoId', {
     templateUrl: 'partials/contato.html',
     controller: 'ContatosController'
   } );
